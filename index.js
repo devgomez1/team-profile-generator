efb8531265c0dde9612268e348f3f2b252193ea1
devin <devinelliotgomez@gmail.com>
2022-12-29 20:16:52 -0600

    added all prompts and functions in the index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Manager = require("./lib/Manager");
-const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
+const genPage = require("./src/htmlGenerator");
 const myTeam = [];
 
 function managerPrompt() {
@@ -44,19 +44,11 @@ function secondInput() {
         type: "list",
         name: "prompt",
         message: "What would you like to do next?",
-        choices: [
-          "Add an Employee",
-          "Add an Intern",
-          "Add an Engineer",
-          "Write File",
-        ],
+        choices: ["Add an Intern", "Add an Engineer", "Write File"],
       },
     ])
     .then((answers) => {
       switch (answers.prompt) {
-        case "Add an Employee":
-          addEmployee();
-          break;
         case "Add an Intern":
           addIntern();
           break;
@@ -69,3 +61,79 @@ function secondInput() {
       }
     });
 }
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the Interns Name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Interns ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Interns email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the Interns school?",
+      },
+    ])
+    .then((data) => {
+      const intern = new Intern(data.name, data.id, data.email, data.school);
+      myTeam.push(intern);
+      secondInput();
+    });
+}
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the Engineers Name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Engineers ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Engineers email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the Engineers github?",
+      },
+    ])
+    .then((data) => {
+      const engineer = new Engineer(
+        data.name,
+        data.id,
+        data.email,
+        data.github
+      );
+      myTeam.push(engineer);
+      secondInput();
+    });
+}
+
+function writeFile() {
+  fs.writeFile("./dist/index.html", genPage(myTeam), (err) => {
+    if (err) throw err;
+    console.log("Your HTML has been generated.");
+  });
+}
+
+managerPrompt();
